adf57d59c9c197eaf61ccbca75d962790b91b69b
Rob Wolfe <robertejohnson10@gmail.com>
2020-08-22 19:53:38 -0500

    added theatre/cat fontawesome fas, changed fas class

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,8 +11,8 @@ export default () => {
     <Navbar variant={toString()} fixed="top" collapseOnSelect expand="md">
       <Navbar.Brand className="pl-5 ml-5" as={Link} to="/">
         <FontAwesomeIcon
-          icon={["fab", `${dark ? "empire" : "rebel"}`]}
-          className={`brand-icon ${dark ? "empire" : "rebel"}`}
+          icon={["fas", `${dark ? "theater-masks" : "cat"}`]}
+          className={`brand-icon ${dark ? "theatre" : ""}`}
           title="Home"
         />
       </Navbar.Brand>
